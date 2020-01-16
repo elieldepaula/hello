0b62332d0cf342361f9b0a538f5eb443884481ea
Eliel de Paula <eliel.depaula@webjump.com.br>
2020-01-16 15:38:41 -0300

    More string

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
-import "fmt"
+
+import (
+  "fmt"
+  "github.com/elieldepaula/hello/morestring"
+)
+
 func main() {
 
-  fmt.Printf("Hello world\n")
+  // fmt.Printf("Hello world\n")
+  fmt.Printf(morestring.ReverseRunes("!oG ,olleH"))
 
 }
